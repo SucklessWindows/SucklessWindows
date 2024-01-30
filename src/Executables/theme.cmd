0976b87b6e872f8186cbbef0fb61cde71e9cfc3d
p3nguin <p3nguinkun@proton.me>
2024-01-30 23:38:28 +0700

    Edit Something
    no desc

--- src/Executables/theme.cmd
+++ src/Executables/theme.cmd
@@ -1,26 +1,12 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-robocopy "Wallpapers" "%windir%\Wallpapers" /E /IM /IT /NP
+robocopy "Wallpapers" "C:\Windows\Wallpapers" /E /IM /IT /NP
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "" /f 
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\Windows\Wallpapers\img.png" /f 
 reg add "HKCU\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
 
-:: Clear lockscreen cache
-for /d %%x in ("!ProgramData!\Microsoft\Windows\SystemData\*") do (
-	for /d %%y in ("%%x\ReadOnly\LockScreen_*") do (
-		rd /s /q "%%y" 
-	)
-)
-
-
-rmdir /q /s "!appdata!\Microsoft\Windows\Themes"
 rundll32.exe user32.dll, UpdatePerUserSystemParameters
-
-@REM if not "%~1"=="AME_UserHive_Default" (
-@REM 	reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Creative\%~1" /v "RotatingLockScreenEnabled" /t REG_DWORD /d "0" /f > nul
-@REM )
-@REM reg add "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "RotatingLockScreenEnabled" /t REG_DWORD /d "0" /f > nul
 
 :: Set sound scheme to 'No Sounds'
 reg add "HKU\%~1\AppEvents\Schemes" /ve /d ".None" /f > nul
