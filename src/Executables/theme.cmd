e4876e4861aa72f2f6aa1033aee5031ee67ff48c
p3nguin <p3nguinkun@proton.me>
2024-01-30 23:53:01 +0700

    Update theme.cmd

--- src/Executables/theme.cmd
+++ src/Executables/theme.cmd
@@ -6,7 +6,7 @@ reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "" /f
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\Windows\Wallpapers\img.png" /f 
 reg add "HKCU\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
 
-rundll32.exe user32.dll, UpdatePerUserSystemParameters
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
 
 :: Set sound scheme to 'No Sounds'
 reg add "HKU\%~1\AppEvents\Schemes" /ve /d ".None" /f > nul
