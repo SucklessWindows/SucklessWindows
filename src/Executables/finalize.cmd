4c320ccac6cfc28015910d862771a2d3de6ade72
p3nguin™ <p3nguinkun@proton.me>
2024-07-14 10:52:35 +0700

    Remove non-sense things

--- src/Executables/finalize.cmd
+++ src/Executables/finalize.cmd
@@ -1,8 +1,5 @@
 @echo off
 set version=1.0
-for /f "tokens=2 delims==" %%i in ('wmic os get BuildNumber /value ^| find "="') do set "build=%%i"
-if %build% gtr 19045 ( set "w11=true" )
-
 
 :: Update Health Tools
 msiexec /X{43D501A5-E5E3-46EC-8F33-9E15D2A2CBD5} /qn /norestart >NUL 2>nul
@@ -38,10 +35,6 @@ schtasks /change /tn "\Microsoft\Windows\UpdateOrchestrator\StartOobeAppsScanAft
 schtasks /change /tn "\Microsoft\Windows\UpdateOrchestrator\Start Oobe Expedite Work" /disable >NUL 2>nul
 schtasks /change /tn "\Microsoft\Windows\UpdateOrchestrator\Schedule Scan" /disable >NUL 2>nul
 
-wevtutil sl Microsoft-Windows-SleepStudy/Diagnostic /q:false >NUL 2>nul
-wevtutil sl Microsoft-Windows-Kernel-Processor-Power/Diagnostic /q:false >NUL 2>nul
-wevtutil sl Microsoft-Windows-UserModePowerService/Diagnostic /q:false >NUL 2>nul
-
 echo Optimizing NTFS settings
 fsutil behavior set disableLastAccess 1 >NUL 2>nul
 fsutil behavior set disable8dot3 1 >NUL 2>nul
@@ -65,17 +58,6 @@ setx DOTNET_CLI_TELEMETRY_OPTOUT 1
 setx POWERSHELL_TELEMETRY_OPTOUT 1
 
 for /f "tokens=2 delims==" %%a in ('wmic os get TotalVisibleMemorySize /format:value') do set "mem=%%a"
-
-for /f "usebackq tokens=2 delims=\" %%a in (`reg query "HKEY_USERS" ^| findstr /r /x /c:"HKEY_USERS\\S-.*" /c:"HKEY_USERS\\AME_UserHive_[^_]*"`) do (
-  REM If the "Volatile Environment" key exists, that means it is a proper user. Built in accounts/SIDs do not have this key.
-  reg query "HKEY_USERS\%%a" | findstr /c:"Volatile Environment" /c:"AME_UserHive_" > nul 2>&1
-    if not errorlevel 1 (
-      if %mem% lss 9000000 ( 
-        reg add "HKU\%%a\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "EnableTransparency" /t REG_DWORD /d "0" /f >NUL
-        reg add "HKU\%%a\Control Panel\Desktop\WindowMetrics" /v "MinAnimate" /t REG_SZ /d "0" /f >NUL 2>nul
-      )
-    )
-  )
 
 :: https://github.com/meetrevision/playbook/issues/15
 :: Updates root certificates
