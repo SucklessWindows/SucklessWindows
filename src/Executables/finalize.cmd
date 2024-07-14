ca2ea80797a78dc3d8b25b0105cdcff6cd48626c
p3nguin™ <p3nguinkun@proton.me>
2024-07-14 10:50:14 +0700

    Remove Ultra Performance power plan for better efficiency

--- src/Executables/finalize.cmd
+++ src/Executables/finalize.cmd
@@ -19,33 +19,7 @@ rmdir /s /q "%ProgramW6432%\\PCHealthCheck" >NUL 2>nul
 "%ProgramFiles(x86)%\WindowsInstallationAssistant\Windows10UpgraderApp.exe" /SunValley /ForceUninstall >NUL 2>nul
 rmdir /s /q "%ProgramFiles(x86)%\WindowsInstallationAssistant" >NUL 2>nul
 
-if not defined w11 (
-	bcdedit /set description "ReviOS 10 %version%" >NUL 2>nul
-  reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v "Model"  /t REG_SZ /d "ReviOS 10 %version%" /f >NUL 2>nul
-  reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "RegisteredOrganization" /t REG_SZ /d "ReviOS 10 %version%" /f >NUL 2>nul
-) else (
-	bcdedit /set description "ReviOS 11 %version%" >NUL 2>nul
-  reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v "Model"  /t REG_SZ /d "ReviOS 11 %version%" /f >NUL 2>nul
-  reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "RegisteredOrganization" /t REG_SZ /d "ReviOS 11 %version%" /f >NUL 2>nul
-)
-
 @REM PowerShell -NonInteractive -NoLogo -NoP -C "Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod" >NUL 2>nul
-
-@REM echo Configuring power settings
-@REM powercfg /hibernate off
-@REM powercfg -restoredefaultschemes
-@REM powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 3ff9831b-6f80-4830-8178-736cd4229e7b
-@REM powercfg -changename 3ff9831b-6f80-4830-8178-736cd4229e7b "Revision - Ultra Performance" "Windows's Ultimate Performance with additional changes."
-@REM powercfg -s 3ff9831b-6f80-4830-8178-736cd4229e7b
-@REM powercfg -setacvalueindex scheme_current sub_processor PERFINCPOL 2
-@REM powercfg -setacvalueindex scheme_current sub_processor PERFDECPOL 1
-@REM powercfg -setacvalueindex scheme_current sub_processor PERFINCTHRESHOLD 10
-@REM powercfg -setacvalueindex scheme_current sub_processor PERFDECTHRESHOLD 8
-@REM powercfg -setacvalueindex scheme_current sub_processor CPMINCORES 100
-@REM powercfg -setacvalueindex scheme_current sub_processor CPMINCORES1 100
-@REM powercfg /setactive scheme_current
-
-@REM PowerShell -NonInteractive -NoLogo -NoP -C "& {$cpu = Get-CimInstance Win32_Processor; $cpuName = $cpu.Name; if ($cpu.Manufacturer -eq 'GenuineIntel') { if ($cpuName.Substring(0, 2) -eq 'In') { Write-Host 'Detected Intel CPU older than 10th generation.' } else { $cpuGen = [int]($cpuName.Substring(0, 2)); if ($cpuGen -gt 11) { Write-Host 'Optimizing Revision''s Ultra powerplan for 12th generation or later Intel CPUs'; powercfg -changename 3ff9831b-6f80-4830-8178-736cd4229e7b 'Revision - Ultra Performance' 'Windows''s Ultimate Performance with optimized settings for newer Intel CPUs.'; powercfg -s 3ff9831b-6f80-4830-8178-736cd4229e7b; powercfg -setacvalueindex scheme_current sub_processor HETEROPOLICY 0; powercfg -setacvalueindex scheme_current sub_processor SCHEDPOLICY 2; powercfg /setactive scheme_current }}};}"
 
 echo Configuring tasks
 schtasks /change /tn "\Microsoft\Windows\Power Efficiency Diagnostics\AnalyzeSystem" /disable >NUL 2>nul
@@ -68,13 +42,6 @@ wevtutil sl Microsoft-Windows-SleepStudy/Diagnostic /q:false >NUL 2>nul
 wevtutil sl Microsoft-Windows-Kernel-Processor-Power/Diagnostic /q:false >NUL 2>nul
 wevtutil sl Microsoft-Windows-UserModePowerService/Diagnostic /q:false >NUL 2>nul
 
-echo Configuring boot settings
-bcdedit /deletevalue useplatformclock >NUL 2>nul
-::bcdedit /set useplatformtick yes >NUL 2>nul
-bcdedit /set disabledynamictick yes >NUL 2>nul
-bcdedit /set bootmenupolicy Legacy >NUL 2>nul
-bcdedit /set lastknowngood yes >NUL 2>nul
-
 echo Optimizing NTFS settings
 fsutil behavior set disableLastAccess 1 >NUL 2>nul
 fsutil behavior set disable8dot3 1 >NUL 2>nul
@@ -96,11 +63,6 @@ powershell -NonInteractive -NoLogo -NoProfile Set-ProcessMitigation -Name vgc.ex
 :: - !run: {exeDir: true, exe: 'powershell -windowstyle hidden -ExecutionPolicy Bypass -C "& ''./MC_PM.ps1''"'}
 setx DOTNET_CLI_TELEMETRY_OPTOUT 1
 setx POWERSHELL_TELEMETRY_OPTOUT 1
-
-echo Configuring animations
-
-:: Breaks XboxGipSvc
-::reg add "HKLM\SYSTEM\CurrentControlSet\Control" /v "SvcHostSplitThresholdInKB" /t REG_DWORD /d "4294967295" /f >NUL
 
 for /f "tokens=2 delims==" %%a in ('wmic os get TotalVisibleMemorySize /format:value') do set "mem=%%a"
 
